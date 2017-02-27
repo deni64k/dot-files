67f56df7ab16e9b12b21dd9fe3e669f6e64dbcdd
Denis Sukhonin <d.sukhonin@gmail.com>
2017-02-27 10:18:33 +0000

    Add a script to manage this stuff.

---
+++ manage.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+install () {
+    return 0
+}
+
+pull () {
+    return 0
+}
+
+status () {
+    find . --exclude ./manage.sh --print --exec diff {} ~/{} \;
+}
+
+case $1 in
+    install)
+        install
+        ;;
+
+    pull)
+        pull
+        ;;
+
+    status)
+        status
+        ;;
+
+    git)
+        shift
+        curdir=$(pwd -P)
+        (cd ~
+         GIT_DIR=$curdir/.git git $*
+        )
+        ;;
+esac
