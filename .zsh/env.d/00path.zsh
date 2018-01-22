2c3349332e3c2f5edf3e6222c0e801ce2429d758
Denis Sukhonin <d.sukhonin@gmail.com>
2018-01-22 01:59:33 +0000

    Remove redundant export from zsh configs

--- .zsh/env.d/00path.zsh
+++ .zsh/env.d/00path.zsh
@@ -1,4 +1,4 @@
-export PATH=/usr/local/bin:/usr/local/sbin:${PATH}
+PATH="/usr/local/bin:/usr/local/sbin:${PATH}"
 [ -d "${HOME}/.cargo/bin" ] && PATH="${HOME}/.cargo/bin:${PATH}"
 [ -d "${HOME}/Code/go/bin" ] && PATH="${HOME}/Code/go/bin:${PATH}"
 [ -d "${HOME}/.bin" ] && PATH="${HOME}/.bin:${PATH}"
