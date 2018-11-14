1b0c33df9687c7d33470f2414ee3cd368749d131
Denis Sukhonin <d.sukhonin@gmail.com>
2018-11-14 22:35:39 +0000

    Bump PATH and MANPATH.

--- .zsh/env.d/00path.zsh
+++ .zsh/env.d/00path.zsh
@@ -1,9 +1,13 @@
-PATH="/opt/local/bin/:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:${PATH}"
+PATH="/usr/local/bin:/usr/local/sbin:${PATH}"
+[ -d "${HOME}/local/bin" ] && PATH="${HOME}/local/bin:${PATH}"
+[ -d "${HOME}/.bin" ] && PATH="${HOME}/.bin:${PATH}"
 [ -d "${HOME}/.cargo/bin" ] && PATH="${HOME}/.cargo/bin:${PATH}"
 [ -d "${HOME}/Code/go/bin" ] && PATH="${HOME}/Code/go/bin:${PATH}"
-[ -d "${HOME}/.bin" ] && PATH="${HOME}/.bin:${PATH}"
-MANPATH="/opt/local/share/man:$MANPATH"
+[ -d "/opt/local/bin" ] && PATH="${PATH}:/opt/local/bin"
 
-export PATH MANPATH
+export PATH
+
+MANPATH="/opt/local/share/man:$MANPATH"
+export MANPATH
 
 typeset -U path cdpath fpath
